e261383098d7fbc7a4d369a61f43089b3deaa337
Jari Haavisto <jari.haavisto@gmail.com>
2020-07-20 11:58:48 +0300

    add local storage

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text, TextInput, FlatList } from 'react-native';
+import { StyleSheet, View, Button, Text, TextInput, FlatList, AsyncStorage } from 'react-native';
+
 
 import initialData from './constants/intial_data'
 import Cocktail from './components/Cocktail'
 import AddCocktailModal from './components/AddCocktailModal';
 
 export default function App() {
-  const [cocktails, setCocktails] = useState(initialData)
+  const [cocktails, setCocktails] = useState([])
   const [searchInput, setSearchInput] = useState('')
   const [showAddCocktail, setShowAddCocktail] = useState(false)
+  const STORAGE_KEY = '@cocktails'
+
+  const saveCocktailData = async (cocktails) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(cocktails))
+      console.log('data saved')
+    } catch (e) {
+      aleconsole.log('Failed to save the data to the storage' + e)
+    }
+  }
+
+  const readCocktailData = async () => {
+    try {
+      const cocktails = await AsyncStorage.getItem(STORAGE_KEY)
+  
+      if (cocktails !== null) {
+        setCocktails(JSON.parse(cocktails))
+      }
+    } catch (e) {
+      console.log('Failed to fetch the data from storage')
+    }
+  }
+
+  React.useEffect(() => {
+    console.log('effect, read data')
+    readCocktailData()
+  }, [])
 
   const scrollTo = (index) => {
     console.log('scrollTo called', { index })
@@ -17,7 +45,9 @@ export default function App() {
 
   const saveCocktail = (cocktail) => {
     const id = 'cocktail_' + cocktails.length
-    setCocktails(cocktails => [...cocktails, { ...cocktail, id }])
+    const newCocktails = [...cocktails, { ...cocktail, id: 'cocktail_' + cocktails.length }]
+    setCocktails(newCocktails)
+    saveCocktailData(newCocktails)
     setShowAddCocktail(false)
   }
 
