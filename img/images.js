ff3b1d60faf5f78ed2886484ac9d7ed696ded72a
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-24 12:54:13 +0300

    add hurricane glass

--- img/images.js
+++ img/images.js
@@ -2,10 +2,12 @@ import dot from './dot.png'
 import coupe from './coupe.png'
 import oldFashioned from './old_fashioned.png'
 import doubleOldFashioned from './double_old_fashioned.png'
+import hurricane from './hurricane.png'
 
 export default {
     'Coupe': coupe,
     'Old Fashioned': oldFashioned,
     'Double Old Fashioned': doubleOldFashioned,
+    'Hurricane': hurricane,
     'dot': dot
 }
