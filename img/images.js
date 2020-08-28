b2da5fec2f8835a3b3833f3e24596ae0e31b3d79
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-28 16:18:00 +0300

    add zombie glass

--- img/images.js
+++ img/images.js
@@ -3,11 +3,13 @@ import coupe from './coupe.png'
 import oldFashioned from './old_fashioned.png'
 import doubleOldFashioned from './double_old_fashioned.png'
 import hurricane from './hurricane.png'
+import zombie from './zombie.png'
 
 export default {
     'Coupe': coupe,
     'Old Fashioned': oldFashioned,
     'Double Old Fashioned': doubleOldFashioned,
     'Hurricane': hurricane,
+    'Zombie': zombie,
     'dot': dot
 }
